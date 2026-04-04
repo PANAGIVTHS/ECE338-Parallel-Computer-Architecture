1c9844a07ff0b1a66113babf9be4ef493a5912f6
TsiantosD <tsiantos.d@gmail.com>
2026-04-04 22:51:03 +0300

    Change assembler.py to create instructions' permutations

--- tb/assembler.py
+++ tb/assembler.py
@@ -2,13 +2,32 @@
 
 from riscv_assembler.convert import AssemblyConverter as AC
 
+import itertools
 
-PROGRAM = """
-addi x1, x1, 7
-add x2, x1, x1
-sub x3, x2, x1
-"""
 
-convert = AC(output_mode = 'p', nibble_mode = True, hex_mode = False)
+def generate_instructions() -> str:
+    program = ""
 
-convert(PROGRAM, "program.hex")
+    # Define registers (x0, ..., x31)
+    registers = [f"x{i}" for i in range(4)]
+
+    immediates = list(range(-10, 11)) + [-2048, 2047]
+
+    # addi
+    for rd, rs1 in itertools.product(registers, repeat=2):
+        for imm in immediates:
+            program += f"addi {rd}, {rs1}, {imm}\n"
+
+    # add
+    for rd, rs1, rs2 in itertools.product(registers, repeat=3):
+        program += f"add {rd}, {rs1}, {rs2}\n"
+
+    # sub
+    for rd, rs1, rs2 in itertools.product(registers, repeat=3):
+        program += f"sub {rd}, {rs1}, {rs2}\n"
+
+    return program
+
+convert = AC(output_mode = 'p', nibble_mode = False, hex_mode = True)
+
+convert(generate_instructions(), "program.hex")
