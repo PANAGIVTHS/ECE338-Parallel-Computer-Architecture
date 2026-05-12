2f1ab1c887890229438fe4f4d969b95bc7d46b98
TsiantosD <tsiantos.d@gmail.com>
2026-05-13 00:00:58 +0300

    Add register x31 as the thread index in adjacent differences

--- programs/differences/differences.c
+++ programs/differences/differences.c
@@ -1,35 +1,45 @@
 #define SIZE 10
+#define NUM_SPS 2
+
+int data[SIZE] = {2, 5, 9, 15, 23, 33, 45, 59, 75, 93};
+int diff[SIZE];
 
 #ifdef __riscv
 int _start() {
-#else
-#include <stdio.h>
-int main() {
-#endif
-    int data[SIZE] = {2, 5, 9, 15, 23, 33, 45, 59, 75, 93};
-    int diff[SIZE];
-    
-    int i;
-    diff[0] = data[0];
+    int threadIdx_x;
 
-    #ifdef __riscv
-    for (i = 1; i < SIZE; i++) {
-        diff[i] = data[i] - data[i-1];
-    }
-    #else
-    for (i = 1; i < SIZE; i++) {
-        diff[i] = data[i] - data[i-1];
-    }
-    #endif
+    // Read the custom hardware register x31 into the variable threadIdx_x
+    __asm__ volatile("mv %0, x31" : "=r"(threadIdx_x));
 
-    #ifdef __riscv
-    #else
-    printf("Index | Data | Adjacent Difference\n");
-    printf("----------------------------------\n");
-    for (i = 0; i < SIZE; i++) {
-        printf("  %2d  |  %2d  |  %2d\n", i, data[i], diff[i]);
+    for (int i = threadIdx_x; i < SIZE; i += NUM_SPS) {
+        if (i == 0) {
+            diff[0] = data[0];
+        } else {
+            diff[i] = data[i] - data[i - 1];
+        }
     }
-    #endif
 
     return 0;
 }
+#else
+#include <stdio.h>
+int main() {
+    for (int threadIdx_x = 0; threadIdx_x < SIZE; threadIdx_x++) {
+        if (threadIdx_x < SIZE) {
+            if (threadIdx_x == 0) {
+                diff[0] = data[0];
+            } else {
+                diff[threadIdx_x] = data[threadIdx_x] - data[threadIdx_x - 1];
+            }
+        }
+    }
+
+    printf("Index | Data | Adjacent Difference\n");
+    printf("----------------------------------\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("  %2d  |  %2d  |  %2d\n", i, data[i], diff[i]);
+    }
+
+    return 0;
+}
+#endif
