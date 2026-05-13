aff10a7c41ef4ada320dd0b06dcbb4d1c7cf0bed
TsiantosD <tsiantos.d@gmail.com>
2026-05-13 22:31:31 +0300

    Remove stack pointer by filtering the instructions, remove frame pointer from the compiler option

--- programs/simple/simple.c
+++ programs/simple/simple.c
@@ -1,15 +1,15 @@
 #ifdef __riscv
-int _start() {
+int indexes_array[2] = {0};
+int ten_array[2] = {0};
+__attribute__((naked)) int _start() {
     int threadIdx_x;
     __asm__ volatile("mv %0, x31" : "=r"(threadIdx_x));
 
     // Test that thread index is written in correct position
-    int indexes_array[2] = {0};
     indexes_array[threadIdx_x] = threadIdx_x;
 
     // Test that both SPs have accessed x and written it to the correct position
     int x = 10;
-    int ten_array[2] = {0};
     ten_array[threadIdx_x] = x;
 }
 #else
