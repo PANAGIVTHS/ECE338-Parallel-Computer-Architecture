96b37803fbd4439004910b8b29f58d4843604219
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2026-05-15 17:11:41 +0300

    fix test 11

--- tb/test11/program.asm
+++ tb/test11/program.asm
@@ -1,5 +1,4 @@
 addi x1, x31, 0 # t0: 0, t1: 1
 slli x1, x1, 2  # t0: 0, t1: 4
-addi x2, x1, 1  # x2 = tidx + 1
+addi x2, x1, 1  # x2 = $t0 + 1
 sw   x2, 0(x1)  # mem[0] = 1, mem[1] = 2
-jalr x0, 0(x1)  # return
